47adc710a93bf50c86fabcaa312255aa361e22f1
brielle-fox <bf161324@ohio.edu>
2026-01-21 20:14:33 -0500

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Brielle Fox
+ *        Date: 1/21/2026
+ * Description: Lab 2 Git program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
